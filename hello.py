c8a676b0539f463f4ff4ea9845ae12e5220f500f
Duyfrom <phamthekhuongduy@gmail.com>
2024-09-11 12:50:13 +0700

    init

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Say hello to user
+#print("Hello, ", end="")
+#print(name)
+
+# Format the text in a string
+print(f"Hello, {name}")
